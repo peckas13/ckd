8555a52b1342f64dbe7354047ab6faac333355b0
USUARIO <alejandroalba999@gmail.com>
2020-04-15 19:28:20 -0500

    componente login

---
+++ App-movil/Frontend/CKD/src/app/login/login-routing.modul.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    children:[
+      {
+        path:'registrar',
+        redirectTo: '/registrar'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LoginPageRoutingModule {}
