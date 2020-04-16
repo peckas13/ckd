bf06c72f593617d1a9faf86a96c8bfdc4d70cb1d
USUARIO <alejandroalba999@gmail.com>
2020-04-15 19:25:37 -0500

    agregado login componente

--- App-movil/Frontend/CKD/src/app/app-routing.module.ts
+++ App-movil/Frontend/CKD/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
